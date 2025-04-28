58c5de7781d00f03a473ab9d1180d88794520cc5
Aman Khan <81584983+amansherjada@users.noreply.github.com>
2025-04-28 17:11:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -168,4 +168,6 @@ def chat():
 # Run Flask App
 # -------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 8080))  # cloud run injects PORT env var
+    app.run(host="0.0.0.0", port=port)
+
